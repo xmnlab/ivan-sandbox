f9c4e1a45fc745e950be96edf2c10f4e73da118a
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-05-03 17:38:10 -0500

    Added conda install for building dependencies

--- pytorch/scripts/2_add_miniconda_to_path.sh
+++ pytorch/scripts/2_add_miniconda_to_path.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export INITIAL_PATH_OLD=$PATH
-export PATH=/home/paperspace/miniconda3/bin:$PATH
+export PATH=/home/xmn/miniconda3/bin:$PATH
--- pytorch/scripts/4_startup_pytorch.sh
+++ pytorch/scripts/4_startup_pytorch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 export CMAKE_PREFIX_PATH=${CONDA_PREFIX:-"$(dirname $(which conda))/../"}
+export DEBUG=1
