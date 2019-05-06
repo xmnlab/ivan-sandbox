519d3eccccf3548c2c30b46658fd750f8471ba90
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2019-05-06 17:30:43 -0500

    Updated scripts

---
+++ pytorch/scripts/3a_create_pytorch_env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# set -ex
+
+export CONDA_ENV_NAME=pytorch-$(whoami)
+conda create -n $CONDA_ENV_NAME -y python=3.6
+
+source activate $CONDA_ENV_NAME
+
+conda install -y -n $CONDA_ENV_NAME cudatoolkit-dev=10.0
+conda install -y -n $CONDA_ENV_NAME numpy pyyaml mkl mkl-include setuptools cmake cffi typing
+conda install -y -n $CONDA_ENV_NAME -c pytorch magma-cuda100
+pip install ninja
+pip install pytest
---
+++ pytorch/scripts/3b_install_conda_build_deps.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env
+conda install -y -n pytorch-$(whoami) cmake libpng jpeg ca-certificates
