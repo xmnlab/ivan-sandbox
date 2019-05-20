d3993bb13db4b9399111890268da22a61e523951
Anthony Scopatz <scopatz@gmail.com>
2019-05-20 10:21:14 -0500

    fix sub

--- pytorch/scripts/start_pytorch_env_gpu.sh
+++ pytorch/scripts/start_pytorch_env_gpu.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-current_dir="$( cd "$( dirname "${{BASH_SOURCE[0]}}" )" >/dev/null 2>&1 && pwd )"
+current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "${current_dir}/source/2_add_miniconda_to_path.sh"
 source "${current_dir}/source/4a_startup_pytorch_conda_env.sh"
 source "${current_dir}/source/4b_startup_pytorch.sh"
